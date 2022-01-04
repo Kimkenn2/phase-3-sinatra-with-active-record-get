36e846a8863d0b83e372b3354051a59c6b631a21
Kenneth Kim <kkm2059@gmail.com>
2022-01-04 11:34:35 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    games = Game.all
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(include: :reviews)
+  end
 end
